571bab50b4584ab22a7b4309fdc2209e5991a00b
emoncse2020 <emon.cse.2020@gmail.com>
2025-03-03 17:22:47 +0600

    migration done db

--- event_ms/settings.py
+++ event_ms/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import dj_database_url
 
 from pathlib import Path
 
@@ -89,18 +90,28 @@ WSGI_APPLICATION = 'event_ms.wsgi.application'
 #     }
 # }
 
+# onrederPOstgres
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://event_ms_db_user:kRCptaLSw5GU3X0fAxcdHWWqMR4xf4Rr@dpg-cv2ou18gph6c73a6kd20-a.oregon-postgres.render.com/event_ms_db',
+        conn_max_age=600
+    )
+}
+
+
 #postgres connection
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'event_ms',
-        'USER': 'postgres',
-        'PASSWORD': 'tasnim',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'event_ms',
+#         'USER': 'postgres',
+#         'PASSWORD': 'tasnim',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 
 
 # Password validation
